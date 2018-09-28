bf5e7b56b6a1de256f9bbd5e75541667b4baf358
Jam <jam.hamidi@maximusbc.ca>
2018-09-28 10:04:34 -0700

    first working version with captch

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
         serviceName: 'Apply for BC Health Care',
         logBaseUrl: '/msp/api/logging',
         apiBaseUrl: '/msp/api',
-        captchaApiBaseUrl: '/msp/api',
+        captchaApiBaseUrl: '/msp/api/captcha',
         addressChangeBCUrl: 'https://www.addresschange.gov.bc.ca/',
         images: {
           maxImagesPerPerson: 50,
