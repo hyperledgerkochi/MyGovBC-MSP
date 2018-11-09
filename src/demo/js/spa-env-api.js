3da9de50cda672e6dc0d5f204c40993d57d2e8d7
Abhimanyu raut <abhimanyu.raut@aot-technologies.com>
2018-11-09 12:42:38 -0800

    Added the header in the request

--- src/demo/js/spa-env-api.js
+++ src/demo/js/spa-env-api.js
@@ -2,6 +2,7 @@ export function callAPIServer(method,url) {
     return new Promise(function (resolve, reject) {
         var xhr = new XMLHttpRequest();
         xhr.open(method, url);
+        xhr.setRequestHeader('SPA_ENV_NAME', '{"SPA_ENV_VIDEO_ASSIST_URL":"","SPA_ENV_AGENT_ID":""}');
         xhr.onload = resolve;
         xhr.onerror = reject;
         xhr.send();
